27924e6e15945cb4144bc6fb411459203173f09c
Ermolenko.K <19symbols@gmail.com>
2021-01-12 19:41:39 +0300

    Настроено отображения в режимах день\ночь

--- SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/ContentView.swift
@@ -8,54 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-      ZStack {
-        BackgroundView(topColor: .blue, bottomColor: Color("lightBlue"))
+  
+  @State private var isNight = false
+  
+  var body: some View {
+    ZStack {
+      BackgroundView(isNight: $isNight)
+      
+      VStack {
+        CityNameView(cityName: "Saint Petersburg")
+        MainWeatherStatusView(isNight: $isNight, temperature: -8)
+        DayForecastSymbols()
         
-        VStack {
-          CityNameView(cityName: "Cupertino, CA")
-          
-          MainWeatherStatusView(imageName: "cloud.sun.fill",
-                                temperature: -8)
-          
-          HStack(spacing: 20) {
-            WeatherDayView(dayOfWeek: "TUE",
-                           imageName: "cloud.hail.fill",
-                           temperature: -15)
-            WeatherDayView(dayOfWeek: "WED",
-                           imageName: "sun.max.fill",
-                           temperature: -11)
-            WeatherDayView(dayOfWeek: "THU",
-                           imageName: "wind.snow",
-                           temperature: -18)
-            WeatherDayView(dayOfWeek: "FRI",
-                           imageName: "snow",
-                           temperature: -13)
-            WeatherDayView(dayOfWeek: "SAT",
-                           imageName: "sun.max.fill",
-                           temperature: -18)
-          }
-
-          Spacer()
-          
-          Button {
-            print("Hello!")
-          } label: {
-            WeatherButton(title: "Change Day Time",
-                          bacgroundColor: .white,
-                          textColor: .blue)
-          }
-          
-          Spacer()
-       }
+        Spacer()
+        
+        Button {
+          isNight.toggle()
+        } label: {
+          WeatherButton(title: "Change Day Time",
+                        bacgroundColor: .white,
+                        textColor: isNight ? .black : .blue)
+        }
+        
+        Spacer()
+      }
     }
   }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
 
 struct WeatherDayView: View {
@@ -67,15 +51,17 @@ struct WeatherDayView: View {
   var body: some View {
     VStack(spacing: 20) {
       Text(dayOfWeek)
-        .font(.system(size: 28, weight: .medium, design: .default))
+        .font(.system(size: 23, weight: .medium, design: .default))
         .foregroundColor(.white)
+      
       Image(systemName: imageName)
         .renderingMode(.original)
         .resizable()
         .aspectRatio(contentMode: .fit)
         .frame(width: 40, height: 40)
+      
       Text("\(temperature)°")
-        .font(.system(size: 28, weight: .medium, design: .default))
+        .font(.system(size: 23, weight: .medium, design: .default))
         .foregroundColor(.white)
     }
   }
@@ -83,11 +69,11 @@ struct WeatherDayView: View {
 
 struct BackgroundView: View {
   
-  var topColor: Color
-  var bottomColor: Color
+  @Binding var isNight: Bool
   
   var body: some View {
-    LinearGradient(gradient: Gradient(colors: [topColor, bottomColor]),
+    LinearGradient(gradient: Gradient(colors: [isNight ? .black : .blue,
+                                               isNight ? .gray : Color("lightBlue")]),
                    startPoint: .topLeading,
                    endPoint: .bottomTrailing)
       .ignoresSafeArea(edges: .all)
@@ -102,17 +88,18 @@ struct CityNameView: View {
     Text(cityName)
       .font(.system(size: 32, weight: .medium, design: .default))
       .foregroundColor(.white)
+      .padding()
   }
 }
 
 struct MainWeatherStatusView: View {
   
-  var imageName: String
+  @Binding var isNight: Bool
   var temperature: Int
   
   var body: some View {
     VStack(spacing: 10) {
-      Image(systemName: imageName)
+      Image(systemName: isNight ? "moon.stars.fill" : "cloud.sun.fill")
         .renderingMode(.original)
         .resizable()
         .aspectRatio(contentMode: .fit)
@@ -123,5 +110,27 @@ struct MainWeatherStatusView: View {
         .foregroundColor(.white)
     }
     .padding(.bottom, 40)
+  }
+}
+
+struct DayForecastSymbols: View {
+  var body: some View {
+    HStack(spacing: 26) {
+      WeatherDayView(dayOfWeek: "TUE",
+                     imageName: "cloud.hail.fill",
+                     temperature: -15)
+      WeatherDayView(dayOfWeek: "WED",
+                     imageName: "sun.max.fill",
+                     temperature: -11)
+      WeatherDayView(dayOfWeek: "THU",
+                     imageName: "wind.snow",
+                     temperature: -10)
+      WeatherDayView(dayOfWeek: "FRI",
+                     imageName: "snow",
+                     temperature: -13)
+      WeatherDayView(dayOfWeek: "SAT",
+                     imageName: "sun.max.fill",
+                     temperature: -16)
+    }
   }
 }
